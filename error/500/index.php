4c6afc75a02c9abd1b1e9f17c08c2f48ecff80ec
Stellaros <chambonniere.paul1033@gmail.com>
2022-05-23 12:50:32 +0200

    Initial Commit - 0.0.2

---
+++ error/500/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<title>SIPRNET | Erreur 500</title>
+
+	<link href="https://fonts.googleapis.com/css?family=Montserrat:700,900" rel="stylesheet">
+	<link type="text/css" rel="stylesheet" href="css/style.css" />
+
+</head>
+
+<body>
+
+	<div id="notfound">
+		<div class="notfound">
+			<div class="notfound-404">
+				<h1>500</h1>
+				<h2>Erreur Système</h2>
+			</div>
+			<a href="/">Page d'accueil</a>
+		</div>
+	</div>
+
+</body>
+
+</html>
